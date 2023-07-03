cb2825602f83e14097a8333ea26c895c66aea499
Omar Jesus Cahuana Rios <omar_jesus23@hotmail.com>
2023-07-02 19:34:00 -0500

    add and styling navBar

--- src/features/components/navbar/NavBar.jsx
+++ src/features/components/navbar/NavBar.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import '../navbar/NavBar.css'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsFillSunFill } from "react-icons/bs";
+import { BsMoonStars } from "react-icons/bs";
+import { BsSun } from "react-icons/bs";
 
-export default function NavBarComponent(){
+export default function NavBarComponent(props){
     return(
-        <h1>NAVBARFUNCA</h1>
+        <>
+        <nav className='nav'>
+            <a href="#" className='brand'>&lt;Omar/&gt;</a>
+            <ul className='navMenu'>
+                <li><a href="#" className='navlink'>Home</a></li>
+                <li><a href="#" className='navlink'>About</a></li>
+                <li><a href="#" className='navlink'>Experience</a></li>
+                <li><a href="#" className='navlink'>Skills</a></li>
+                <li><a href="#" className='navlink'>Projects</a></li>
+                <li><a href="#" className='navlink'>Contact</a></li>
+            </ul>
+            <BsFillMoonStarsFill/>
+            <BsMoonStars/>
+            <BsFillSunFill/>
+            <BsSun/>
+            <div className='navHamburguer'>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
+            </div>
+        </nav> 
+        </>
+        
     )
 }
