6b70ce329297908ec274fbc23af1836d124e092a
Omar Jesus Cahuana Rios <omar_jesus23@hotmail.com>
2023-10-11 16:02:42 -0500

    completing section about me

--- src/features/components/home/Home.jsx
+++ src/features/components/home/Home.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import Yo from '../body/Yo.png';
+import './Home.css';
 
 export default function HomeComponent(){
     return(
-        <h1>AEA Y TU TESIS?</h1>
+        <div class="imagen">
+            <div>
+            <img src={Yo} class="fotoperfil" alt="" />
+
+            </div>
+          
+          <div className="descripcion">
+            <p>¡Hola! Soy Omar. Ingeniero de Software, soy de Perú y tengo 25 años. Cuento con más de 2 años de experiencia enfocado al desarrollo Front. Entre mis pasatiempos tengo:</p>
+            <ul>
+                <li>Viajar</li>
+                <li></li>
+                <li>Escuchar música</li>
+                <li></li>
+                <li>Videojuegos</li>
+                <li></li>
+                <li>Series y películas</li>
+                <li></li>
+                <li>Caminar</li>
+                <li></li>   
+                <li>Pet Lover</li>
+            </ul>
+          </div>
+        </div>
     )
 }
 
